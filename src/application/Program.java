d7c38ef99cfe6104017e1d5cf81d5dbb16251703
Julio Cesar <kucip2234@gmail.com>
2023-11-08 17:33:14 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,22 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
 		Department obj = new Department(1, "books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(1, "Julio", "julio@email.",new Date(), 3500.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
